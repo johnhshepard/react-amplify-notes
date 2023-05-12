7cbc054fa4f76d81c5146b7b22a3d3eb0385e0a3
John Shepard <john@gnymble.com>
2023-05-12 00:29:05 -0400

    Update authorization on Notes model to owner.

--- src/models/schema.js
+++ src/models/schema.js
@@ -53,15 +53,6 @@ export const schema = {
                     "properties": {
                         "rules": [
                             {
-                                "allow": "public",
-                                "operations": [
-                                    "create",
-                                    "update",
-                                    "delete",
-                                    "read"
-                                ]
-                            },
-                            {
                                 "provider": "userPools",
                                 "ownerField": "owner",
                                 "allow": "owner",
